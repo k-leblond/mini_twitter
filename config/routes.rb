af2d56146692eb3ee17e7992bf96151d873d44f3
Kevin Leblond <leblond_kevin@hotmail.fr>
2022-03-28 19:43:00 +0200

    Added followers

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :posts, only: %i[new create destroy]
+
+  resources :users do
+    member do
+    get :following, :followers
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
