5a3584a1d271f8c91c4e7ee7c870c3123257e042
Kevin Leblond <leblond_kevin@hotmail.fr>
2022-03-31 16:41:16 +0200

    Discover index WIP

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :posts, only: %i[new create destroy]
+  resources :posts, only: %i[new create destroy index]
 
   resources :users do
     member do
-    get :following, :followers
+      get :following, :followers
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
