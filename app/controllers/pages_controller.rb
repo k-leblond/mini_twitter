d62867d4e2ce1e0bb22123e0f3c1c65a68cee61b
Kevin Leblond <leblond_kevin@hotmail.fr>
2022-04-09 01:58:00 +0200

    Added css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   def home
     @post = Post.new
     following_ids = current_user.following.ids+[current_user.id]
-    @posts = Post.where(user_id: following_ids).paginate(page: params[:page], per_page: 10)
+    @posts = Post.where(user_id: following_ids).paginate(page: params[:page], per_page: 2)
   end
 
 end
