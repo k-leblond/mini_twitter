1617a52944020db8493a05e8ba679bb0c4cfa718
Kevin Leblond <leblond_kevin@hotmail.fr>
2022-04-08 17:02:40 +0200

    Fixed likes

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,27 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    protected
+
+    def find_verified_user
+      if (current_user = env["warden"].user)
+        current_user
+      elsif (token = decoded_token)
+        User.find(token[:user_id])
+      else
+        reject_unauthorized_connection
+      end
+    end
+
+    def decoded_token
+      JsonWebToken.decode(request.params[:token])
+    rescue StandardError => e
+      reject_unauthorized_connection
+    end
   end
 end
--- app/reflexes/likes_reflex.rb
+++ app/reflexes/likes_reflex.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
-
 class LikesReflex < ApplicationReflex
 
   def upvote
     post = Post.find(element.dataset[:id])
-    post.find_or_create_by(user: current_user)
-    if post.find
-      post.destroy
+    like_find = Like.find_by(post: post, user: current_user)
+    if like_find
+      like_find.destroy
     else
-      post.like.user = current_user
-      post.save!
+      like = Like.new
+      like.post = post
+      like.user = current_user
+      like.save!
     end
 
   end
